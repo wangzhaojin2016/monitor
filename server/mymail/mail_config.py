974322cc576644a30a241d5bd721228b847b7193
wangzhaojin2016 <1036484177@qq.com>
2016-03-18 17:24:08 +0800

    Update mail_config.py

--- server/mymail/mail_config.py
+++ server/mymail/mail_config.py
@@ -2,10 +2,10 @@
 
 ##############default setting ####################
 mail_title  ="Hadoop监控"                         #邮件主题
-mail_to_list=['ljyw@teamtopgame.com']             #收件人
+mail_to_list=['xx.com']                           #收件人
 
 ################smtp setting ######################
 mail_host="smtp.qq.com"                               #设置smtp服务器
-mail_user="ywjk@teamtopgame.com"                      #用户名
+mail_user="ywjk@xx.com"                               #用户名
 mail_pass="TEAMdsfdsffff"                             #密码 
 mail_postfix="zhaojin.com"                            #发件箱的后缀
